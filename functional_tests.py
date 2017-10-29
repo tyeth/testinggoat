9d158746c37bb9fb73619173ac7dc55573f14b89
tyeth <tyethgundry@gmail.com>
2017-10-29 20:19:59 +0000

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -42,7 +42,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')  
         self.assertTrue(
-            any(row.text == '1: Make america great again' for row in rows)
+            any(row.text == '1: Make america great again' for row in rows),
+			"New to-do item did not appear in table"
         )
         
         # There is still the todo entry fieldset, he enters "Make VScreen great Again"
