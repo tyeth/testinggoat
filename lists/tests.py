b7bbe9fdc40c6d3aab69e8e6c70c73b6ba8b6cd0
tyeth <tyethgundry@gmail.com>
2017-10-30 13:44:48 +0000

    Revert "seperated concerns in post test"
    This reverts commit 3fe4fa3e4ebba1f1988f19a0c5e6186196e33799.

--- lists/tests.py
+++ lists/tests.py
@@ -41,11 +41,8 @@ class HomepageTest(TestCase):
 	#todo: Code smell - post test too long
 	def test_can_save_a_post_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
-		
 		self.assertEqual(Item.objects.count(),1)
-		
 		new_item = Item.objects.first()
-		
 		self.assertEqual(new_item.text,'A new list item')
 		
 		#self.assertIn('A new list item', response.content.decode())
@@ -53,15 +50,6 @@ class HomepageTest(TestCase):
 		
 	def test_redirects_after_POST(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
-		
 		self.assertEqual(response.status_code, 302)
 		self.assertEqual(response['location'],'/')
 		
-	def test_displays_all_list_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-		
-		response = self.client.get('/')
-		
-		self.assertIn('itemey 1', response.content.decode())
-		self.assertIn('itemey 2', response.content.decode())
