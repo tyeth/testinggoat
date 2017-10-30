2e41f2ff4c0aca54f07af6465d3558eb762ce1b6
tyeth <tyethgundry@gmail.com>
2017-10-30 13:26:49 +0000

    test with working name, breaks with capitol POST like in demo

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-
+	#todo: stop saving an empty item each page load
+	#todo: display multiple items in the table
+	#todo: support more than one list
+	if request.method=='POST':
+		new_item_text = request.POST.get('item_text','')
+		Item.objects.create(text=new_item_text)
+	else:
+		new_item_text=''
+	
 	return render(request,'home.html', {
-		'new_item_text': request.POST.get('item_text'), 
+		'new_item_text': new_item_text, 
 		})
